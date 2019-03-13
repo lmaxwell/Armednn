2b288605fe26bdaa58d83cfe8fbd702e83fb3cea
fanpengustc <fangpeng@mail.ustc.edu.cn>
2019-03-13 17:34:18 +0800

    modify debug log

--- src/core/common.cpp
+++ src/core/common.cpp
@@ -3,10 +3,10 @@
 namespace Armednn
 {
 
-#if __DEBUG__
+#ifndef NDEBUG
 bool LogMessage::_enable=true;
 #else
-bool LogMessage::_enable=true;
+bool LogMessage::_enable=false;
 #endif
 
 }//namespace Armednn
